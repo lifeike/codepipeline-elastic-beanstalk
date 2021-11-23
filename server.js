17db19d42db47e796c856068b67667b997d5a29a
lifeike <lifeike@gmail.com>
2021-11-23 13:47:45 -0700

    serve static files

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express")
+const path = require("path")
 const app = express()
 
 const port = process.env.HTTP_PORT || 8080
+
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 app.get("/", function (req, res) {
   res.send("hello, this is backend ")
